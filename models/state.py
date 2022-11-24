b80c82183b336909934a8b88ebe73aa86b015a81
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-24 14:08:07 +0100

    adding nw methods

--- models/state.py
+++ models/state.py
@@ -3,6 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, Sequence, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
@@ -11,5 +14,14 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable = False)
 
     cities = relationship("City",  backref="state", cascade="delete")
+    if getenv("HBNB_TYPE_STORAGE") != "db":
 
-    
+        @property
+        def cities(self):
+            """"returns the list of City instances"""
+            listofcities = []
+            allcititesinstance = models.storage.all(City)
+            for i in allcititesinstance.values():
+                if i.state_id == self.id:
+                    listofcities.append(i)
+            return listofcities
