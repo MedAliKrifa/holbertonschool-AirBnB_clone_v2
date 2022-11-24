ece4a3dd40aecf7dd8b409d0d479a554e7bd79bb
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-24 22:36:17 +0100

    fixing

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-
+from models.engine.file_storage import FileStorage
+from os import getenv
+from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 
-storage = FileStorage()
-storage.reload()
+
