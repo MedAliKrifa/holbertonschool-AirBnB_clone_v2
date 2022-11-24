339ff81806a3061d7360e18d10a6e88c08fa8b3e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-24 21:09:38 +0100

    hasbeya allah fik ya justin

--- models/state.py
+++ models/state.py
@@ -9,15 +9,15 @@ from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = "states"
-
-        name = Column(String(128), nullable = False)
-
-
-        cities = relationship("City",  backref="state")
     
-    else:
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable = False)
+
+
+    cities = relationship("City",  backref="state")
+    
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """"returns the list of City instances"""
