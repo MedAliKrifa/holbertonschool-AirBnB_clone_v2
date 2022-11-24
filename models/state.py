fee4de2bf2597469c0f935684206158a32f5416e
Mahdi3Bani <bani.mahdi666@gmail.com>
2022-11-24 12:19:36 +0100

    adding nw methods

--- models/state.py
+++ models/state.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, Sequence, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "State"
+
+    name = Column(String(128), nullable = False)
+
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    
